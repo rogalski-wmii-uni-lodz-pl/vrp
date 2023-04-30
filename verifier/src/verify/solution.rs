f2f4ec45f596709935b98c58e6fe0a95dd982086
Mark Rogalski <marek.rogalski@wmii.uni.lodz.pl>
2023-04-30 20:51:04 +0200

    Add solution parsing tests, fix instance_name rule

--- verifier/src/verify/solution.rs
+++ verifier/src/verify/solution.rs
@@ -11,7 +11,7 @@ use std::str::FromStr;
 pub struct SolutionParser;
 
 #[serde_with::serde_as]
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Solution {
     pub instance_name: String,
     pub routes: Vec<Vec<usize>>,
@@ -75,8 +75,69 @@ impl FromStr for Solution {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
     #[test]
-    fn it_works() {}
+    fn read_gh_solution() {
+        let sol_str = concat!(
+            "Instance name: rc1_4_10\n",
+            "Authors: \n",
+            "Date:\n",
+            "Reference: \n",
+            "Solution\n",
+            "Route 1: 1 2 3\n",
+            "Route 2: 4 5 6\n",
+            "Route 3: 7\n",
+            "Route 3: 8 9 10 11 12\n"
+        );
+
+        let sol = Solution::from_str(sol_str);
+
+        assert_eq!(sol.as_ref().err(), None);
+
+        assert_eq!(
+            sol.unwrap(),
+            Solution {
+                instance_name: "rc1_4_10".to_string(),
+                routes: vec![
+                    vec![1, 2, 3],
+                    vec![4, 5, 6],
+                    vec![7],
+                    vec![8, 9, 10, 11, 12],
+                ],
+            }
+        );
+    }
+
+    #[test]
+    fn read_ll_solution() {
+        let sol_str = concat!(
+            "Instance name: LR2_8_1\n",
+            "Authors: \n",
+            "Date:\n",
+            "Reference: \n",
+            "Solution\n",
+            "Route 1: 1 2 3\n",
+            "Route 2: 4 5 6\n",
+            "Route 3: 7\n",
+            "Route 3: 8 9 10 11 12\n"
+        );
+
+        let sol = Solution::from_str(sol_str);
+
+        assert_eq!(sol.as_ref().err(), None);
+
+        assert_eq!(
+            sol.unwrap(),
+            Solution {
+                instance_name: "LR2_8_1".to_string(),
+                routes: vec![
+                    vec![1, 2, 3],
+                    vec![4, 5, 6],
+                    vec![7],
+                    vec![8, 9, 10, 11, 12],
+                ],
+            }
+        );
+    }
 }
