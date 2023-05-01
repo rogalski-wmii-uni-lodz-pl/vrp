2d4a42b73ae7c3c8812d869421d04e76eb6c75cc
Mark Rogalski <marek.rogalski@wmii.uni.lodz.pl>
2023-05-01 08:52:23 +0200

    Parse args

--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -3,19 +3,21 @@ pub mod verify;
 pub use verify::instance;
 pub use verify::solution;
 
+use rug;
 use std::fs::read_to_string;
 use std::path::Path;
 use std::str::FromStr;
-use rug;
 
-fn read<T : FromStr<Err=String>>(path: &Path) -> Result<T, String> {
+fn read<T: FromStr<Err = String>>(path: &Path) -> Result<T, String> {
     let f = read_to_string(path).map_err(|x| x.to_string())?;
 
     T::from_str(&f)
 }
 
-
-pub fn check_sintef_file(path: &Path, instances_loc: &Path) -> Result<(solution::Solution, rug::Float), String> {
+pub fn check_sintef_file(
+    path: &Path,
+    instances_loc: &Path,
+) -> Result<(solution::Solution, rug::Float), String> {
     let solution = read::<solution::Solution>(path)?;
     let instance_path = instances_loc.join(&solution.instance_name);
     let instance = read::<instance::Instance>(&instance_path)?;
--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -1,11 +1,25 @@
 use std::env;
+use std::path::PathBuf;
 use verifier;
-use std::path::Path;
+
+struct Args {
+    solution_path: PathBuf,
+    instances_location: PathBuf,
+}
+
+impl Args {
+    fn from_env() -> Self {
+        let args: Vec<_> = env::args().collect();
+        Args {
+            solution_path: PathBuf::from(&args[1]),
+            instances_location: PathBuf::from(if args.len() < 2 { "." } else { &args[2] }),
+        }
+    }
+}
 
 fn main() -> Result<(), String> {
-    let args : Vec<_> = env::args().collect();
-    let sol_path = Path::new(&args[1]);
-    let (sol, res) = verifier::check_sintef_file(sol_path, Path::new("./"))?;
+    let args = Args::from_env();
+    let (sol, res) = verifier::check_sintef_file(&args.solution_path, &args.instances_location)?;
 
     println!("{} {} {}", sol.instance_name, sol.routes.len(), res);
     Ok(())
