946d9903a1692d475bdbbbd7f8be075f21c598ea
Mark Rogalski <marek.rogalski@wmii.uni.lodz.pl>
2023-05-01 08:30:31 +0200

    More parsing tests

--- verifier/src/verify/solution.rs
+++ verifier/src/verify/solution.rs
@@ -51,7 +51,9 @@ impl FromStr for Solution {
         for r in parsed.into_inner() {
             match r.as_rule() {
                 Rule::instance_name => {
-                    instance_name = r.as_span().as_str().to_owned();
+                    let mut s = r.as_span().as_str().to_owned();
+                    s.retain(|c| !c.is_whitespace());
+                    instance_name = s;
                 }
                 Rule::route => {
                     routes.push(
@@ -88,7 +90,7 @@ mod tests {
             "Route 1: 1 2 3\n",
             "Route 2: 4 5 6\n",
             "Route 3: 7\n",
-            "Route 3: 8 9 10 11 12\n"
+            "Route 4: 8 9 10 11 12\n"
         );
 
         let sol = Solution::from_str(sol_str);
@@ -120,7 +122,7 @@ mod tests {
             "Route 1: 1 2 3\n",
             "Route 2: 4 5 6\n",
             "Route 3: 7\n",
-            "Route 3: 8 9 10 11 12\n"
+            "Route 4: 8 9 10 11 12\n"
         );
 
         let sol = Solution::from_str(sol_str);
@@ -139,5 +141,193 @@ mod tests {
                 ],
             }
         );
+    }
+
+    #[test]
+    fn save_solution() {
+        let sol = Solution {
+            instance_name: "LC1_8_7".to_string(),
+            routes: vec![vec![7, 8], vec![9, 10, 11], vec![5, 4, 3, 2, 1], vec![6]],
+        };
+        let today = chrono::Local::now().format("%Y-%m-%d");
+        assert_eq!(
+            sol.to_string(),
+            format!(
+            "Instance name: LC1_8_7\nAuthors: \nDate: {today}\nReference: \nSolution\nRoute 1: 7 8\nRoute 2: 9 10 11\nRoute 3: 5 4 3 2 1\nRoute 4: 6\n"
+            )
+        );
+    }
+
+    #[test]
+    fn whitespaces_are_ok() {
+        let sol_str = concat!(
+            "Instance name\t:  \t   rc1_4_10\n",
+            "Authors : \n",
+            "Date:\n",
+            "Reference    : \n",
+            "Solution\n",
+            "Route 1: 1   2     3\n",
+            "Route    2   : 4\t5 6\n",
+            "Route\t 3: 7\n",
+            "Route 4: 8 9\t10     11\t12         \n"
+        );
+
+        let sol = Solution::from_str(sol_str);
+
+        assert_eq!(sol.as_ref().err(), None);
+
+        assert_eq!(
+            sol.unwrap(),
+            Solution {
+                instance_name: "rc1_4_10".to_string(),
+                routes: vec![
+                    vec![1, 2, 3],
+                    vec![4, 5, 6],
+                    vec![7],
+                    vec![8, 9, 10, 11, 12],
+                ],
+            }
+        );
+    }
+
+    #[test]
+    fn nonsequential_routes() {
+        let sol_str = concat!(
+            "Instance name: rc1_4_10\n",
+            "Authors: \n",
+            "Date:\n",
+            "Reference: \n",
+            "Solution\n",
+            "Route 0: 1 2 3\n",
+            "Route 7 : 4 5 6\n",
+            "Route 3: 7\n",
+            "Route: 8 9 10 11 12\n"
+        );
+
+        let sol = Solution::from_str(sol_str);
+
+        assert_eq!(sol.as_ref().err(), None);
+
+        assert_eq!(
+            sol.unwrap(),
+            Solution {
+                instance_name: "rc1_4_10".to_string(),
+                routes: vec![
+                    vec![1, 2, 3],
+                    vec![4, 5, 6],
+                    vec![7],
+                    vec![8, 9, 10, 11, 12],
+                ],
+            }
+        );
+    }
+
+    #[test]
+    fn whitespace_in_instance_name() {
+        let sol_str = concat!(
+            "Instance name: rc 1 _ \t 4_10\n",
+            "Authors: \n",
+            "Date:\n",
+            "Reference: \n",
+            "Solution\n",
+            "Route 0: 1 2 3\n",
+        );
+
+        let sol = Solution::from_str(sol_str);
+
+        assert_eq!(sol.as_ref().err(), None);
+
+        assert_eq!(
+            sol.unwrap(),
+            Solution {
+                instance_name: "rc1_4_10".to_string(),
+                routes: vec![vec![1, 2, 3],],
+            }
+        );
+    }
+
+    #[test]
+    fn no_instance_name() {
+        let sol_str = concat!(
+            "Instance name:\n",
+            "Authors: \n",
+            "Date:\n",
+            "Reference: \n",
+            "Solution\n",
+            "Route 0: 1 2 3\n",
+        );
+
+        let sol = Solution::from_str(sol_str);
+
+        assert_eq!(sol.as_ref().err(), None);
+
+        assert_eq!(
+            sol.unwrap(),
+            Solution {
+                instance_name: "".to_string(),
+                routes: vec![vec![1, 2, 3],],
+            }
+        );
+    }
+
+    #[test]
+    fn whatever_in_other_fields() {
+        let sol_str = concat!(
+            "Instance name:\n",
+            "Authors: my pet hamster\n",
+            "Date: whatever 2023-13-72\n",
+            "Reference: 中文范例文本نص مثال عربي\n",
+            "Solution\n",
+            "Route 0: 1 2 3\n",
+        );
+
+        let sol = Solution::from_str(sol_str);
+
+        assert_eq!(sol.as_ref().err(), None);
+
+        assert_eq!(
+            sol.unwrap(),
+            Solution {
+                instance_name: "".to_string(),
+                routes: vec![vec![1, 2, 3],],
+            }
+        );
+    }
+
+    #[test]
+    fn good_instance_names() {
+        for pdp in ["", "l"] {
+            for t in ["c1", "c2", "r1", "r2", "rc1", "rc2"] {
+                for s in (2..=10).step_by(2) {
+                    for i in 1..=10 {
+                        let inst = format!("{pdp}{t}_{s}_{i}");
+                        assert!(SolutionParser::parse(Rule::instance_name, &inst).is_ok());
+                        let inst = format!("{pdp}{t}_{s}_{i}").to_uppercase();
+                        assert!(SolutionParser::parse(Rule::instance_name, &inst).is_ok());
+                    }
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn bad_instance_names() {
+        assert!(SolutionParser::parse(Rule::instance_name, "l").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "rc").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "lrc").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r_2_10").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r1_1_10").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "m1_1_10").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r1_m_10").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r1_2_xyz").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r1_5_10").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r1_2_").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "r2210").is_err());
+        assert!(SolutionParser::parse(Rule::instance_name, "lr2210").is_err());
+
+        assert!(SolutionParser::parse(Rule::instance_name, "r1_2_12").is_ok());
+        // this is technically ok according to instance_name, but next will fail to parse:
+        assert!(SolutionParser::parse(Rule::instance, "Instance name: r1_2_12\n").is_err());
     }
 }
