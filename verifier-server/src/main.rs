ecd3570a318570b98e211696b0d8191dcae0ab2f
Mark Rogalski <marek.rogalski@wmii.uni.lodz.pl>
2023-05-02 10:52:20 +0200

    Read best known results

--- verifier-server/src/main.rs
+++ verifier-server/src/main.rs
@@ -1,11 +1,15 @@
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use clap::Parser;
+use rug;
 use std::collections::HashMap;
+use std::fs;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use verifier::read;
 use verifier::verify::verify;
-use verifier::{instance::Instance, solution::Solution};
+use verifier::instance::{fl, Instance};
+use verifier::solution::Solution;
+use walkdir;
 
 type InstancesDb = HashMap<String, Instance>;
 
@@ -25,7 +29,7 @@ async fn checker(data: web::Data<Db>, req_body: String) -> impl Responder {
             }
             Some(instance) => {
                 let resp = match verify(&instance, &solution) {
-                    Ok(dist) => dist.to_string(),
+                    Ok(dist) => format!("{} {} {}", instance.name, solution.routes.len(), dist),
                     Err(err) => err,
                 };
 
@@ -54,12 +58,33 @@ fn read_instances(instances_dir: &Path) -> Result<InstancesDb, std::io::Error> {
     Ok(db)
 }
 
+#[derive(Debug)]
+struct Bks {
+    routes: usize,
+    distance: rug::Float,
+    // date
+    // who
+}
+
+impl Bks {
+    fn new() -> Self {
+        Bks {
+            routes: usize::MAX,
+            distance: fl(0),
+        }
+    }
+}
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// path to the location of instances to read
+    /// path to the directory containing instances
     #[arg(short, long)]
     instances_dir: PathBuf,
+
+    /// path to the directory containing best known solutions
+    #[arg(short, long)]
+    bks_dir: Option<PathBuf>,
 
     /// port to bind to
     #[arg(short, long, default_value_t = 8080)]
@@ -72,6 +97,28 @@ async fn main() -> std::io::Result<()> {
 
     println!("starting, listening on {}", args.port);
     let db = read_instances(&args.instances_dir)?;
+
+    let mut bks: HashMap<String, Bks> = HashMap::new();
+
+    if let Some(bks_dir) = args.bks_dir {
+        for b in walkdir::WalkDir::new(bks_dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|f| f.file_type().is_file())
+            .filter(|f| fs::metadata(f.path()).unwrap().len() > 0)
+        {
+            let sol = read::<Solution>(b.path()).unwrap();
+            let inst = db.get(&sol.instance_name).unwrap();
+            *bks.entry(sol.instance_name).or_insert(Bks::new()) = Bks { routes: sol.routes.len(), distance: verify(&inst, &sol).unwrap() };
+        }
+    }
+
+    println!("read {} bks", bks.len());
+
+    for (name, b) in bks.iter() {
+        println!("{:10} : {:3} {}", name, b.routes, b.distance);
+    }
+
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(Db {
